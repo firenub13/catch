9fbeabbe1384aac9e3c73c78b572cc0cacca5c7e
mushroom_0291 <98500312+firenub13@users.noreply.github.com>
2025-04-23 11:08:38 -0500

    Add files via upload

--- SUMMATIVE-Catch 'em/MyWorld.java
+++ SUMMATIVE-Catch 'em/MyWorld.java
@@ -10,6 +10,8 @@ public class MyWorld extends World
 {
     GreenfootSound cheering = new GreenfootSound("cheering.mp3");
     GreenfootSound jockey = new GreenfootSound("jockey.mp3");
+    int chickenDone = 0;
+    int chickenStarted = 0;
     //Constructor for the MyWorld
         //code that is run when the program starts
     public MyWorld()
@@ -23,6 +25,18 @@ public class MyWorld extends World
     //act method - loops when the run button is pressed
     public void act(){
         //add actors here
+        if (chickenStarted == 0){
+            chickenStarted = 1;
+            jockey.play();
+        }
+        if (jockey.isPlaying()){
+            chickenDone = 0;
+        }else{
+            chickenDone = 1;
+        }
         
+        if (chickenDone == 1){
+            cheering.play();
+        }
     }
 }
